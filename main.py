8cb7ec7b5754f4a19f882d9e25b8518e58116dff
xAdrianCx <adrian.cotuna26@gmail.com>
2022-11-03 20:23:50 +0200

    Reworked snake.py(created update method).

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+
 import pygame
 from settings import Game_Settings
 from snake import Snake
@@ -14,20 +14,22 @@ def run_game():
 
     # Create a screen.
     screen = pygame.display.set_mode((gs.screen_width, gs.scrren_height))
-    caption = pygame.display.set_caption("Snake Game")
+    pygame.display.set_caption("Snake Game")
     screen.fill(gs.bg_color)
 
     # Instantiate the snake.
-    snake = Snake()
+    snake = Snake(gs, screen)
 
     # Main loop.
     while True:
         for event in pygame.event.get():
-            if event == pygame.QUIT:
-                sys.exit()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == K_UP:
+                    snake.up_arrow = True
         screen.fill(gs.bg_color)
         snake.draw_snake()
         pygame.display.flip()
-
 
 run_game()
--- snake.py
+++ snake.py
@@ -4,7 +4,7 @@ import pygame
 class Snake():
     """A class that models a snake"""
 
-    def __init_(self, gs, screen):
+    def __init__(self, gs, screen):
         """Initialize snake attributes"""
         self.gs = gs
         self.screen = screen
@@ -13,12 +13,25 @@ class Snake():
         # Position the snake in the middle of the screen.
         self.rect.centerx = self.screen_rect.centerx
         self.rect.centery = self.screen_rect.centery
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
 
+        self.up_arrow = False
+        self.down_arrow = False
+        self.left_arrow = False
+        self.right_arrow = False
 
     def update(self):
         """A method that updates the snake position."""
-        pass
+        if self.up_arrow and self.y >= self.screen_rect.top:
+            self.y -= self.gs.snake_speed
+        if self.down_arrow and self.y <= self.screen_rect.bottom:
+            self.y += self.gs.snake_speed
+        if self.left_arrow and self.x >= self.screen_rect.left:
+            self.x -= self.gs.snake_speed
+        if self.right_arrow and self.x <= self.screen_rect.right:
+            self.x += self.gs.snake_speed
 
     def draw_snake(self):
         """Draws the snake on the screen"""
-        pygame.draw.rect(self.screen, self.gs.color, self.rect)
+        pygame.draw.rect(self.screen, self.gs.snake_color, self.rect)
